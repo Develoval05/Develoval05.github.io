7cb1f526bc662f3dd6494cd61cb2985ed6a23e28
Develoval05 <121402155+Develoval05@users.noreply.github.com>
2025-07-14 22:48:16 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
-header("./namfod/public/index.html");
-exit();
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="0;url=nueva_pagina.php">
+  <title>Redireccionando...</title>
+</head>
+<body>
+  Si no eres redirigido automáticamente, <a href="./namfod/public/index.html">haz clic aquí</a>.
+</body>
+</html>
+
